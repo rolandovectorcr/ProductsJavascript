f2763ddcaf32c396f6230264b5e5269d437bd13e
rolandovectorcr <rolando.arguello.email@@gmail.com>
2019-06-10 01:54:14 -0600

    Last commit

--- app.js
+++ app.js
@@ -7,28 +7,38 @@ class Product {
 
 }
 
-class UI{
+class UI {
     addproduct(product) {
         const productList = document.getElementById('product-list');
         const element = document.createElement('div');
-        element.innerHTML = '<div class="card text-center mb-4"><div class="card-body"><strong>Name: ' + 
-        product.name + '</strong><br><strong>Price: ' + 
-        product.price + '</strong><br><strong>Year: ' + 
-        product.year + '</strong></div></div>'
-        + '<a href="#" class="btn btn-danger" name="delete">Delete</a>';
+        element.innerHTML = '<div class="card text-center mb-4"><div class="card-body"><strong>Name: ' +
+            product.name + '</strong><br><strong>Price: ' +
+            product.price + '</strong><br><strong>Year: ' +
+            product.year + '</strong></div></div>'
+            + '<a href="#" class="btn btn-danger" name="delete">Delete</a>';
         productList.appendChild(element);
         this.clearform();
     }
-    clearform(){
+    clearform() {
         document.getElementById('product-form').reset();
     }
     deleteproduct(element) {
-        if(element.name === 'delete'){
+        if (element.name === 'delete') {
             element.parentElement.parentElement.parentElement.remove();
+            this.ShowMessage('Producto eliminado', 'danger');
         }
     }
-    ShowMessage() {
-
+    ShowMessage(message, cssClass) {
+        const div = document.createElement('div');
+        div.className = 'alert alert-' + cssClass;
+        div.appendChild(document.createTextNode(message));
+        //Showing in the DOM
+        const container = document.querySelector('.container');
+        const app = document.querySelector('#App');
+        container.insertBefore(div, app);
+        setTimeout(function () {
+            document.querySelector('.alert').remove();
+        }, 3000);
     }
 }
 
@@ -40,14 +50,20 @@ document.getElementById("product-form")
         const year = document.getElementById('year').value;
         const product = new Product(name, price, year);
         const ui = new UI();
+
+        if(name === "" || price === "" || year === ""){
+            return ui.ShowMessage("Terminar de llenar los datos", 'danger');
+        }
+
         ui.addproduct(product);
+        ui.clearform();
+        ui.ShowMessage('Producto Agreagado satisfactoriamente');
+
         e.preventDefault();
 
     });
 
-    document.getElementById("product-list").addEventListener('click', function(e){
-        const ui = new UI();
-        ui.deleteproduct(e.target);
-
-        
-    });
+document.getElementById("product-list").addEventListener('click', function (e) {
+    const ui = new UI();
+    ui.deleteproduct(e.target);
+});
